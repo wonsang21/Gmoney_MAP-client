b5b41e04534fb4f787250cdc981030c777b7627f
wonsangcho <wonsangcho@wonsangchoui-MacBookPro.local>
2020-08-11 03:15:28 +0900

    Fix AddressDialog.js

--- src/components/map/AddressDialog.js
+++ src/components/map/AddressDialog.js
@@ -1,6 +1,7 @@
 /* global kakao */
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, forwardRef } from 'react';
+import { withRouter } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -16,10 +17,6 @@ import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
 
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const useStyles = makeStyles((theme) => ({
   appBar: {
     position: 'relative',
@@ -32,24 +29,55 @@ const useStyles = makeStyles((theme) => ({
     width: '98%',
     margin: '10px 10px 10px 10px',
   },
-  searchBtn: {
+  searchInput: {
     width: '40%',
     marginRight: '10px',
     borderRadius: '4px 4px 4px 4px',
+    float: 'left',
+  },
+  searchBtn: {
+    width: '10%',
+    height: '56px',
+    marginRight: '10px',
+    borderRadius: '4px 4px 4px 4px',
+    float: 'left',
   },
   searchDivider: {
     backgroundColor: 'black',
   },
+  paging: {
+    width: '100%',
+    textAlign: 'center',
+    backgroundColor: 'black',
+  },
+  pagination: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  pageBtn: {
+    display: 'block',
+    marginLeft: '10px',
+  },
+  pageBtnClick: {
+    fontWeight: 'bold',
+    cursor: 'default',
+    color: '#777',
+  },
 }));
 
-const Transition = React.forwardRef(function Transition(props, ref) {
+const Transition = forwardRef(function Transition(props, ref) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-const AddressDialog = () => {
+const AddressDialog = (props) => {
   const classes = useStyles();
+  const ps = new kakao.maps.services.Places(); // 장소 검색 객체를 생성합니다
+  const { handleLocation } = props;
+
   const [open, setOpen] = useState(false);
+  const [places, setPlaces] = useState(null); // 검색결과 장소
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -57,90 +85,99 @@ const AddressDialog = () => {
 
   const handleClose = () => {
     setOpen(false);
+    setPlaces(null);
   };
 
+  useEffect(() => {
+    console.log('새로운 검색결과입니다.', places);
+  }, [places]);
+
+  // 검색결과 목록 하단에 페이지번호를 표시는 함수입니다 (미완료)
+  const displayPagination = (pagination) => {
+    const paginationEl = document.getElementById('pagination');
+    const fragment = document.createDocumentFragment();
+    let i;
+
+    // 기존에 추가된 페이지번호를 삭제합니다
+    while (paginationEl.hasChildNodes()) {
+      paginationEl.removeChild(paginationEl.lastChild);
+    }
+
+    // eslint-disable-next-line no-plusplus
+    for (i = 1; i <= pagination.last; i++) {
+      const el = document.createElement('a');
+      el.className = classes.pageBtn;
+      el.href = '#';
+      el.innerHTML = i;
+
+      if (i === pagination.current) {
+        el.className = classes.pageBtnClick;
+      } else {
+        // eslint-disable-next-line no-shadow
+        el.onclick = (function (i) {
+          return function () {
+            pagination.gotoPage(i);
+          };
+        })(i);
+      }
+
+      fragment.appendChild(el);
+    }
+    paginationEl.appendChild(fragment);
+  };
+
+  // 검색 결과 목록을 표출하는 함수입니다
+  // eslint-disable-next-line consistent-return
+  const displayPlaces = () => {
+    if (places !== null) {
+      return places.map((place) => (
+        <>
+          <ListItem button divider>
+            <ListItemText
+              primary={place.place_name}
+              secondary={
+                place.road_address_name
+                  ? place.road_address_name
+                  : place.address_name
+              }
+              onClick={() => {
+                handleLocation(place);
+                handleClose();
+                props.history.push('/');
+              }}
+            />
+          </ListItem>
+        </>
+      ));
+    }
+  };
+
+  // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다 (완료)
+  const placesSearchCB = (data, status, pagination) => {
+    if (status === kakao.maps.services.Status.OK) {
+      // 정상적으로 검색이 완료됐으면
+      // 검색 목록을 state로 끌어올립니다.
+      setPlaces(data);
+
+      // 페이지 번호를 표출합니다
+      displayPagination(pagination);
+    } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+      alert('검색 결과가 존재하지 않습니다.');
+    } else if (status === kakao.maps.services.Status.ERROR) {
+      alert('검색 결과 중 오류가 발생했습니다.');
+    }
+  };
+
+  // 키워드 검색을 요청하는 함수입니다 (완료)
   const searchPlaces = () => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API}&autoload=false&libraries=services,clusterer,drawing`;
-    document.head.appendChild(script);
+    const keyword = document.getElementById('keyword').value;
 
-    script.onload = () => {
-      kakao.maps.load(() => {
-        // 장소 검색 객체를 생성합니다
-        const ps = new kakao.maps.services.Places();
+    if (!keyword.replace(/^\s+|\s+$/g, '')) {
+      alert('키워드를 입력해주세요!');
+    }
 
-        // 검색결과 목록의 자식 Element를 제거하는 함수입니다
-        const removeAllChildNods = (el) => {
-          while (el.hasChildNodes()) {
-            el.removeChild(el.lastChild);
-          }
-        };
-
-        // 검색 결과 목록을 표출하는 함수입니다
-        const displayPlaces = (places) => {
-          const listEl = document.getElementById('placesList');
-          const menuEl = document.getElementById('menu_wrap');
-          const fragment = document.createDocumentFragment();
-
-          // 검색 결과 목록에 추가된 항목들을 제거합니다
-          removeAllChildNods(listEl);
-
-          // 검색결과 항목들을 검색결과 목록 Elemnet에 추가합니다
-          listEl.appendChild(fragment);
-          menuEl.scrollTop = 0;
-        };
-
-        // 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
-        const displayPagination = (pagination) => {
-          const paginationEl = document.getElementById('pagination');
-          const fragment = document.createDocumentFragment();
-          const i = 0;
-
-          // 기존에 추가된 페이지번호를 삭제합니다
-          while (paginationEl.hasChildNodes()) {
-            paginationEl.removeChild(paginationEl.lastChild);
-          }
-
-          for (i = 1; i <= pagination.last; i++) {
-            var el = document.createElement('a');
-            el.href = '#';
-            el.innerHTML = i;
-
-            if (i === pagination.current) {
-              el.className = 'on';
-            } else {
-              el.onclick = (function (i) {
-                return function () {
-                  pagination.gotoPage(i);
-                };
-              })(i);
-            }
-
-            fragment.appendChild(el);
-          }
-          paginationEl.appendChild(fragment);
-        };
-
-        // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
-        const placesSearchCB = (data, status, pagination) => {
-          if (status === kakao.maps.services.Status.OK) {
-            // 정상적으로 검색이 완료됐으면
-            // 검색 목록과 마커를 표출합니다
-            displayPlaces(data);
-
-            // 페이지 번호를 표출합니다
-            displayPagination(pagination);
-          } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-            alert('검색 결과가 존재하지 않습니다.');
-            return;
-          } else if (status === kakao.maps.services.Status.ERROR) {
-            alert('검색 결과 중 오류가 발생했습니다.');
-            return;
-          }
-        };
-      });
-    };
+    // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
+    ps.keywordSearch(keyword, placesSearchCB);
   };
 
   return (
@@ -153,7 +190,7 @@ const AddressDialog = () => {
           readOnly: true,
         }}
         variant="outlined"
-        fullWidth="true"
+        fullWidth
         onClick={handleClickOpen}
       />
       <Dialog
@@ -178,32 +215,35 @@ const AddressDialog = () => {
           </Toolbar>
         </AppBar>
         <div className={classes.searchBar}>
-          <div className={classes.searchBtn}>
-            <TextField
-              id="outlined-input"
-              label="ex) 효원로 1, 롯데리아.."
-              variant="outlined"
-              autoFocus="true"
-              fullWidth="true"
-            />
-          </div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              searchPlaces();
+            }}
+          >
+            <div className={classes.searchInput}>
+              <TextField
+                id="keyword"
+                label="ex) 효원로 1, 롯데리아.."
+                variant="outlined"
+                autoFocus
+                fullWidth
+              />
+            </div>
+            <div className={classes.searchBtn}>
+              <Button type="submit">검색</Button>
+            </div>
+          </form>
         </div>
+
         <Divider className={classes.searchDivider} />
-        <List id="placesList">
-          <ListItem button>
-            <ListItemText primary="Phone ringtone" secondary="Titania" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText
-              primary="Default notification ringtone"
-              secondary="Tethys"
-            />
-          </ListItem>
-        </List>
+        <List>{displayPlaces()}</List>
+        <div className={classes.paging}>
+          <div id="pagination" className={classes.pagination}></div>
+        </div>
       </Dialog>
     </>
   );
 };
 
-export default AddressDialog;
+export default withRouter(AddressDialog);
